a7db234c718c2948315ec20d1cea89fae2765540
carlosnunezbaro <127980966+carlosnunezbaro@users.noreply.github.com>
2025-04-02 19:49:47 +0200

    Update Disparo.h

--- Disparo.h
+++ Disparo.h
@@ -1,8 +1,8 @@
 #ifndef Disparo_h_INCLUDED
 #define Disparo_h_INCLUDED
 
-int disparoJ1(int tablero,int taboponente1,int turnos,BarcosJ2,int nbJ2,int fin);
-int disparoIA(int tablero,int taboponente2,int turnos,BarcosJ2,int nbJ1,int fin);
-
+int disparoJ1(char **tablero2,int turnos,BarcosJ2,int nbJ2,int fin);
+int disparoIA(char **tablero1,int turnos,BarcosJ1,int nbJ1,int fin);
+void inicializador();
 
 #endif // Disparo_h_INCLUDED
