0da6ed0b5a9cb640691c30da0b62673671c8f059
carlosnunezbaro <127980966+carlosnunezbaro@users.noreply.github.com>
2025-04-09 19:33:35 +0200

    Update Disparo.h

--- Disparo.h
+++ Disparo.h
@@ -1,9 +1,11 @@
 #ifndef Disparo_h_INCLUDED
 #define Disparo_h_INCLUDED
 
-int disparoJ1(char **tablero2,int turnos,BarcosJ2,int nbJ2,int fin);
-int disparoIA(char **tablero1,int turnos,BarcosJ1,int nbJ1,int fin);
+void disparoJ1(int filas, int columnas, char **tablero2, char **tabop2, int *turnos, Barcos *barcosJ2, int *nbJ2, int *fin);
+void disparoJ2(int filas, int columnas, char **tablero1, char **tabop1, int *turnos, Barcos *barcosJ1, int *nbJ1, int *fin);
+void disparoIA(char **tablero1, char **tabop1, int filas, int columnas, int *turnos, Barcos *barcosJ1, int *nbJ1, int *fin);
 void inicializador();
-void chequeoH(char **tablero[i][j],int nBar,Barcos);
+void chequeoH(int i, int j, char **tablero, char **tabop, Barcos *barcos, int *nBar) ;
 
 #endif // Disparo_h_INCLUDED
+
