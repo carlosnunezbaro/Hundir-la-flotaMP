7c21bf83e8b012e2c6c6f264accffc993c5025a5
carlosnunezbaro <127980966+carlosnunezbaro@users.noreply.github.com>
2025-04-16 11:04:07 +0200

    Corregido un par de cosas en cuanto a sintaxis con el resto de modulos

--- Disparo.h
+++ Disparo.h
@@ -1,11 +1,11 @@
 #ifndef Disparo_h_INCLUDED
 #define Disparo_h_INCLUDED
 
-void disparoJ1(int filas, int columnas, char **tablero2, char **tabop2, int *turnos, Barcos *barcosJ2, int *nbJ2, int *fin);
-void disparoJ2(int filas, int columnas, char **tablero1, char **tabop1, int *turnos, Barcos *barcosJ1, int *nbJ1, int *fin);
-void disparoIA(char **tablero1, char **tabop1, int filas, int columnas, int *turnos, Barcos *barcosJ1, int *nbJ1, int *fin);
+void disparoJ1(int filas, int columnas, char **j2_Flota, char **j2_Oponente, int *turnos, Barcos *barcosJ2, int *nbJ2, int *fin);
+void disparoJ2(int filas, int columnas, char **j1_Flota, char **j1_Oponente, int *turnos, Barcos *barcosJ1, int *nbJ1, int *fin);
+void disparoIA(char **j1_Flota, char **j1_Oponente, int filas, int columnas, int *turnos, Barcos *barcosJ1, int *nbJ1, int *fin);
 void inicializador();
-void chequeoH(int i, int j, char **tablero, char **tabop, Barcos *barcos, int *nBar) ;
+void chequeoH(int i, int j, char **j_Flota, char **j_Oponente, Barcos *barcos, int *nBar);
 
 #endif // Disparo_h_INCLUDED
 
