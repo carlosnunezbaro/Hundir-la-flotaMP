a95c20a32a0a9be646f460bd7d0011a0d3695ec1
José Blanco Cerpa <74268278+joseBC06@users.noreply.github.com>
2025-04-21 15:14:31 +0200

    Update and rename main to main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "configuracion_inicial.h"
+#include "juego.h"
+#include "Disparo.h"
+#include "estado-barcos.h"
+
+
+int main(){
+
+    configuracion_inicial();
+
+
+
+
+
+
+}
