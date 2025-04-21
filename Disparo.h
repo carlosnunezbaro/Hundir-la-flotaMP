c6009817b54fb843c4411a94e6fde76638dc9c39
carlosnunezbaro <127980966+carlosnunezbaro@users.noreply.github.com>
2025-04-21 16:23:37 +0200

    Update Disparo.h

--- Disparo.h
+++ Disparo.h
@@ -1,11 +1,11 @@
 #ifndef Disparo_h_INCLUDED
 #define Disparo_h_INCLUDED
 
-void disparoJ1(int filas, int columnas, char **j2_Flota, char **j2_Oponente, int *turnos, Barcos *barcosJ2, int *nbJ2, int *fin);
-void disparoJ2(int filas, int columnas, char **j1_Flota, char **j1_Oponente, int *turnos, Barcos *barcosJ1, int *nbJ1, int *fin);
-void disparoIA(char **j_Flota, char **j_Oponente, int filas, int columnas, int *turnos, Barcos *barcosJ, int *nbJ, int *fin);
+void disparoJ1(int filas, int columnas, char **j2_Flota, char **j2_Oponente, int *turnos, Barco *barcosJ2, int *nbJ2, int *fin);
+void disparoJ2(int filas, int columnas, char **j1_Flota, char **j1_Oponente, int *turnos, Barco *barcosJ1, int *nbJ1, int *fin);
+void disparoIA(char **j_Flota, char **j_Oponente, int filas, int columnas, int *turnos, Barco *barcosJ, int *nbJ, int *fin);
 void inicializador();
-void chequeoH(int i, int j, char **j_Flota, char **j_Oponente, Barcos *barcos, int *nBar);
+void chequeoH(int i, int j, char **j_Flota, char **j_Oponente, Barco *barcos, int *nBar);
 
 #endif // Disparo_h_INCLUDED
 
