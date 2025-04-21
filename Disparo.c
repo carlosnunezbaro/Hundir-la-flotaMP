879f83fdfa49c689188478d5732233444eb67216
carlosnunezbaro <127980966+carlosnunezbaro@users.noreply.github.com>
2025-04-21 16:24:36 +0200

    Update Disparo.c

--- Disparo.c
+++ Disparo.c
@@ -2,18 +2,19 @@
 #include<stdlib.h>
 #include "Disparo.h"
 #include "estado-barcos.h"
-#include "barcos.h"
 #include "juego.h"
+#include "configuracion_inicial.h"
 
 
 void inicializador(){
+    int *num_barcos;
     int turnos = 1;
     int fin = 1;
     int *nbJ1 = *num_barcos;
     int *nbJ2 = *num_barcos;
 }
 
-void disparoJ1(int filas, int columnas, char **j2_Flota, char **j2_Oponente, int *turnos, Barcos *barcosJ2, int *nbJ2, int *fin){
+void disparoJ1(int filas, int columnas, char **j2_Flota, char **j2_Oponente, int *turnos, Barco *barcosJ2, int *nbJ2, int *fin){
     int i,j = 0;
     printf("introduce la posicion del disparo \n");
     do{
@@ -26,7 +27,7 @@ void disparoJ1(int filas, int columnas, char **j2_Flota, char **j2_Oponente, int
         printf("fallo");
         j2_Flota[i][j]='*';
         j2_Oponente[i][j]='*';
-        *disparosJ1 = *disparosJ1+1;
+        *disparoJ1 = *disparoJ1+1;
         printf("%d",j2_Oponente[i][j]);   // imprime la matriz oponente con el efecto del disparo ya efectuado
     }else{
         do{
@@ -48,9 +49,9 @@ void disparoJ1(int filas, int columnas, char **j2_Flota, char **j2_Oponente, int
                     printf("fallo");
                     j2_Flota[i][j]='*';
                     j2_Oponente[i][j]='*';
-                    printf("%d",j2_Oponente[i][j]); 
+                    printf("%d",j2_Oponente[i][j]);
                      }
-                }while(j2_Flota[i][j]=='B'||j2_Flota[i][j]!='*' )
+                }while(j2_Flota[i][j]=='B'||j2_Flota[i][j]!='*');
 
         }
 
@@ -59,7 +60,7 @@ void disparoJ1(int filas, int columnas, char **j2_Flota, char **j2_Oponente, int
         fin = 0;
     }
 }
-void disparoJ2(int filas, int columnas, char **j1_Flota, char **j1_Oponente, int *turnos, Barcos *barcosJ1, int *nbJ1, int *fin){
+void disparoJ2(int filas, int columnas, char **j1_Flota, char **j1_Oponente, int *turnos, Barco *barcosJ1, int *nbJ1, int *fin){
     int i,j = 0;
     printf("introduce la posicion del disparo \n");
     do{
@@ -94,9 +95,9 @@ void disparoJ2(int filas, int columnas, char **j1_Flota, char **j1_Oponente, int
                     printf("fallo");
                     j1_Flota[i][j]='*';
                     j1_Oponente[i][j]='*';
-                    printf("%d",j1_Oponente[i][j]); 
-                     }
-                }while(j1_Flota[i][j]=='B'||j1_Flota[i][j]!='*' )
+                    printf("%d",j1_Oponente[i][j]);
+                     };
+                }while(j1_Flota[i][j]=='B'||j1_Flota[i][j]!='*');
 
         }
 
@@ -107,11 +108,11 @@ void disparoJ2(int filas, int columnas, char **j1_Flota, char **j1_Oponente, int
 }
 
 
-void disparoIA(char **j_Flota, char **j_Oponente, int filas, int columnas, int *turnos, Barcos *barcosJ, int *nbJ, int *fin){
-int i,j,ci,cj;
+void disparoIA(char **j_Flota, char **j_Oponente, int filas, int columnas, int *turnos, Barco *barcosJ, int *nbJ, int *fin){
+int i,j,ci,cj,n;
 i=0;
 j=0;
-while(i==0||i>=10){    \\ selecciona una posicion aleatoria de la matriz
+while(i==0||i>=10){    // selecciona una posicion aleatoria de la matriz
     i=rand()%11;
 }
 while(j==0||j>=10){
@@ -120,7 +121,7 @@ while(j==0||j>=10){
     if(jugador1.disparo='A'){
         j_Flota[i][j] = j1_Flota[i][j];
         j_Oponente[i][j] = j1_oponente[i][j];
-        *nbj = *nbJ1;
+        *nbJ = *nbJ1;
         *disparosJ1 =  *disparosJ1+1;
     }else{
         j_Flota[i][j] = j2_Flota[i][j];
@@ -130,7 +131,7 @@ while(j==0||j>=10){
     }
  if(j_Flota[i][j]=='-'){
        printf("fallo");
-        j_Flota[i][j]='*'; 
+        j_Flota[i][j]='*';
         j_Oponente[i][j]='*';
         printf("%d",j_Oponente[i][j]);
     }else{
@@ -144,45 +145,43 @@ while(j==0||j>=10){
                     ci=i;
                     cj=j;
                     for(n=0;n<=100;n++){
-                    while(ci<=i-1||ci==i||ci>=i+1){     \\ selecciona una posicion aleatoria de un area de la matriz donde ha ocurrido un impacto
+                    while(ci<=i-1||ci==i||ci>=i+1){     // selecciona una posicion aleatoria de un area de la matriz donde ha ocurrido un impacto
                         i=rand()%11;
                         }
                         while(cj<=j-1||cj==j||cj>=j+1){
                         j=rand()%11;
-                        }
                         if(jugador1.disparo='A'){
                         *disparosJ1 =  *disparosJ1+1;
                             }else{
                         *disparosJ2 =  *disparosJ2+1;
                             }
-                if(j_Flota[i][j]=='B'||j_Flota[i][j]=!'*'){
-                    printf("tocado");
-                    j_Flota[i][j]='T';
-                    j_Oponente[i][j]='T';
-                    printf("%d",j_Oponente[i][j]);
-                    chequeoH(j_Flota[i][j],j_Oponente[i][j],barcosJ,nbJ);
-                    printf("%d",j_Oponente[i][j]);
+                        }
+                if(j_Flota[i][j]=='B'){
+                    if(j_Flota[i][j]=!'*'){
+                        printf("tocado");
+                        j_Flota[i][j]='T';
+                        j_Oponente[i][j]='T';
+                        printf("%d",j_Oponente[i][j]);
+                        chequeoH(j_Flota[i][j],j_Oponente[i][j],barcosJ,nbJ);
+                        printf("%d",j_Oponente[i][j]);
+                    }else{
+                        printf("fallo");
+                        j_Flota[i][j]='*';
+                        j_Oponente[i][j]='*';
+                        printf("%d",j_Oponente[i][j]);
                    }
-                }else{
-                    printf("fallo");
-                    j_Flota[i][j]='*';
-                    j_Oponente[i][j]='*';
-                    printf("%d",j_Oponente[i][j]);
                 }
-                
+
                 }
-            }while(j_Flota[i][j]=='B'||j_Flota[i][j]=!'*')
+            }while(j_Flota[i][j]=='B'|| j_Flota[i][j]=!'*');
 
             }
-        }
-
-    }
-    turnos++;
+        turnos++;
     if(nbJ=0){
         fin = 0;
     }
-}
-void chequeoH(int i, int j, char **j_Flota, char **j_Oponente, Barcos *barcos, int *nBar) {
+
+void chequeoH(int i, int j, char **j_Flota, char **j_Oponente, Barco *barcos, int *nBar) {
     int h, l;
     int hay_B = 0;
 
@@ -191,8 +190,8 @@ void chequeoH(int i, int j, char **j_Flota, char **j_Oponente, Barcos *barcos, i
         for (l = j - 1; l <= j + 1; l++) {
             if (h >= 0 && l >= 0 && j_Flota[h] != NULL && j_Flota[h][l] != '\0') {
                 if (j_Flota[h][l] == 'B') {
-                    j_Flota[h][l] = 'T'; 
-                    hay_B = 1;         
+                    j_Flota[h][l] = 'T';
+                    hay_B = 1;
                 }
             }
         }
@@ -202,15 +201,16 @@ void chequeoH(int i, int j, char **j_Flota, char **j_Oponente, Barcos *barcos, i
     if (!hay_B) {
         for (h = i - 1; h <= i + 1; h++) {
             for (l = j - 1; l <= j + 1; l++) {
-                
+
                 if (h >= 0 && l >= 0 && j_Flota[h] != NULL && j_Flota[h][l] != '\0') {
                     if (j_Flota[h][l] == 'T') {
-                        j_Flota[h][l] = 'H'; 
-                        j_Oponente[h][l] = 'H'; 
-                        (*nBar)--; 
+                        j_Flota[h][l] = 'H';
+                        j_Oponente[h][l] = 'H';
+                        (*nBar)--;
                     }
                 }
             }
         }
     }
 }
+
